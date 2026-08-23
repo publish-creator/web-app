15c1b5864f6a1b8a291bbf0035ebab1a4b289d25
techionext <tech@ionext.com.br>
2026-08-23 12:33:21 -0300

    Update formatCurrency function to accept dynamic currency parameter instead of hardcoded 'USD'

--- src/utils/format-currency.ts
+++ src/utils/format-currency.ts
@@ -1,7 +1,7 @@
-export const formatCurrency = (amount: number, currency?: string = 'USD') => {
+export const formatCurrency = (amount: number, currency: string = 'USD') => {
   const value = amount / 100;
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'USD',
+    currency,
   }).format(value);
 };
